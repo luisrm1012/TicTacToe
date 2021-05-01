bf0374e758ba1d683c08196022bc63541aefd414
Luis Rasquin <luisrm1012@gmail.com>
2021-05-01 14:21:16 +0200

    Modal and play again

--- app.js
+++ app.js
@@ -43,35 +43,37 @@ function checkWin(x) {
 
     //Check borders
     if (board[0] == x && board[1] == x && board[2] == x) {
-        alert("Player " + x + " You Win!!!")
+        //WIN
+        showWinner(x)
     } else if (board[0] == x && board[3] == x && board[6] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     } else if (board[6] == x && board[7] == x && board[8] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     } else if (board[2] == x && board[5] == x && board[8] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     }
     //Check center
     else if (board[1] == x && board[4] == x && board[7] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     } else if (board[3] == x && board[4] == x && board[5] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     }
     //Check diagonals
     else if (board[0] == x && board[4] == x && board[8] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     } else if (board[2] == x && board[4] == x && board[6] == x) {
         //WIN
-        alert("Player " + x + " You Win!!!")
+        showWinner(x)
     }
+}
 
-
-
-
+function showWinner (x){
+    document.getElementById("modal").style.display = "flex"
+    document.getElementById("winner").innerHTML = "Player " + x + " Wins!!!"
 }
